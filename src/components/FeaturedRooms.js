1f2ee081d4347d43d1a86c7135f098f2bf28cb23
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-02-17 23:02:17 +0530

    first Commit

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { RoomContext } from "../context";
+
+export default class FeaturedRooms extends Component {
+  static contextType = RoomContext;
+  render() {
+    const { greeting, name } = this.context;
+
+    return (
+      <div>
+        hello from featured room {greeting} {name}{" "}
+      </div>
+    );
+  }
+}
---
+++ src/context.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+const RoomContext = React.createContext();
+
+// <RoomContext.Provider value={'hello'}
+
+class RoomProvider extends Component {
+  state = {
+    greeting: "hello",
+    name: "Sumit"
+  };
+  render() {
+    return (
+      <RoomContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </RoomContext.Provider>
+    );
+  }
+}
+
+const RoomConsumer = RoomContext.Consumer;
+
+export { RoomProvider, RoomConsumer, RoomContext };
